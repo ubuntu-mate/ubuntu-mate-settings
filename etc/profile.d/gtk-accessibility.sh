de224bc6f349180fe8349292cf9e63f8861855e1
Martin Wimpress <martin@ubuntu-mate.org>
2017-07-18 21:04:35 +0100

    Drop gtk-accessibility.sh

--- etc/profile.d/gtk-accessibility.sh
+++
@@ -1,7 +0,0 @@
-# Ensure GTK accessibility modules are active.
-if [ -z "$GTK_MODULES" ]; then
-    GTK_MODULES=gail:atk-bridge
-else
-    GTK_MODULES="$GTK_MODULES:gail:atk-bridge"
-fi
-export GTK_MODULES
