70a0f2bd478302d8a82392ae329c7d132d64e1ac
Martin Wimpress <martin@ubuntu-mate.org>
2017-07-18 21:13:11 +0100

    Export appropriate enviroment variable via /etc/profile.d

--- etc/profile.d/mate.sh
+++ etc/profile.d/mate.sh
@@ -1,12 +1,8 @@
 # MATE Desktop theme integrations and tweaks
 if [ "x$DESKTOP_SESSION" == "xmate" ] || [ "x$XDG_SESSION_DESKTOP" == "xmate" ]; then
-    # Disable GTK3 overlay scrollbars
-    export GTK_OVERLAY_SCROLLING=0
-
     # QT apps to use GTK styling
     export QT_QPA_PLATFORMTHEME=gtk2
 
-    # Change to GDK_SCALE=2 for experimental HiDPI support.
-    # Requires reboot.
-    export GDK_SCALE=1
+    # Qt automatic scaling, based on the pixel density of the monitor.
+    export QT_AUTO_SCREEN_SCALE_FACTOR=1
 fi
