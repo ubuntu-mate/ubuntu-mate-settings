4d9e92648b7ebd137bb83bd5ed7a24824dea9268
Martin Wimpress <martin@ubuntu-mate.org>
2017-07-18 21:10:10 +0100

    Revert "Consolidate all MATE integrations in /etc/proffile.d"
    This reverts commit 22fea8ee5ee192ef1decc63f2785597f0dd626b2.

--- etc/profile.d/mate.sh
+++ etc/profile.d/mate.sh
@@ -1,25 +1,12 @@
+# MATE Desktop theme integrations and tweaks
 if [ "x$DESKTOP_SESSION" == "xmate" ] || [ "x$XDG_SESSION_DESKTOP" == "xmate" ]; then
-    # Ensure GTK accessibility modules are active.
-    if [ -z "$GTK_MODULES" ]; then
-        GTK_MODULES=gail:atk-bridge:canberra-gtk-module
-    else
-        GTK_MODULES="$GTK_MODULES:gail:atk-bridge:canberra-gtk-module"
-    fi
-    export GTK_MODULES
-
     # Disable GTK3 overlay scrollbars
     export GTK_OVERLAY_SCROLLING=0
 
-    # For experimental HiDPI scaling change to:
-    #   GDK_SCALE=2
-    #   GDK_DPI_SCALE=0.5
-    # Requires a reboot.
-    export GDK_SCALE=1
-    export GDK_DPI_SCALE=1
-
-    # Qt apps will use GTK styling
+    # QT apps to use GTK styling
     export QT_QPA_PLATFORMTHEME=gtk2
 
-    # Qt automatic scaling, based on the pixel density of the monitor.
-    export QT_AUTO_SCREEN_SCALE_FACTOR=1
+    # Change to GDK_SCALE=2 for experimental HiDPI support.
+    # Requires reboot.
+    export GDK_SCALE=1
 fi
