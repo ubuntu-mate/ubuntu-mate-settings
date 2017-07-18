75c8441ec9567f2fec993091c664d10787bc837e
Martin Wimpress <martin@ubuntu-mate.org>
2017-07-18 21:15:29 +0100

    Rename mate-sh to mate-qt.sh and comment appropriately.

--- etc/profile.d/mate-qt.sh
+++ etc/profile.d/mate-qt.sh
@@ -1,4 +1,4 @@
-# MATE Desktop theme integrations and tweaks
+# MATE Desktop Qt integrations
 if [ "x$DESKTOP_SESSION" == "xmate" ] || [ "x$XDG_SESSION_DESKTOP" == "xmate" ]; then
     # QT apps to use GTK styling
     export QT_QPA_PLATFORMTHEME=gtk2
