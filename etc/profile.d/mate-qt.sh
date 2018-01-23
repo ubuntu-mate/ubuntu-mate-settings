3ade1952b026b8c61d55ca8684cd3973afe11e31
Martin Wimpress <martin@ubuntu-mate.org>
2018-01-23 16:51:59 +0000

    Drop HiDPI tweaks, this will be managed by MATE Tweak.

--- etc/profile.d/mate-qt.sh
+++ etc/profile.d/mate-qt.sh
@@ -2,9 +2,4 @@
 if [ "x$DESKTOP_SESSION" == "xmate" ] || [ "x$XDG_SESSION_DESKTOP" == "xmate" ]; then
     # QT apps to use GTK styling
     export QT_QPA_PLATFORMTHEME=gtk2
-
-    # Qt automatic scaling, based on the pixel density of the monitor.
-    # Not enabled by default because it scales Qt applications on
-    # 13" 1080p displays
-    # export QT_AUTO_SCREEN_SCALE_FACTOR=1
 fi
