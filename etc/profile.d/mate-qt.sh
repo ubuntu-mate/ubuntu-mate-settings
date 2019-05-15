4e4bc48b5df3ee718a95d052ff1471594a0c4b19
Ahmed Masud <ahmed.masud@trustifier.com>
2019-05-15 13:15:55 -0400

    This fixes a warning during login when /bin/dash is /bin/sh
    Problem:
    
    dash does not support == as an operator for [ ], which in-turn causes
    a warning about /etc/profile failing when starting up an X session.

--- etc/profile.d/mate-qt.sh
+++ etc/profile.d/mate-qt.sh
@@ -1,5 +1,5 @@
 # MATE Desktop Qt integrations
-if [ "x$DESKTOP_SESSION" == "xmate" ] || [ "x$XDG_SESSION_DESKTOP" == "xmate" ]; then
+if [ "x$DESKTOP_SESSION" = "xmate" ] || [ "x$XDG_SESSION_DESKTOP" = "xmate" ]; then
     # QT apps to use GTK styling
     export QT_QPA_PLATFORMTHEME=gtk2
 fi
